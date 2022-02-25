2cec953839846957632357959ceae03f3081ab03
Jordan <jordan@chude.co>
2022-02-24 22:29:51 -0500

    uncomment submit artist and connect to db

--- app.js
+++ app.js
@@ -14,23 +14,23 @@ app.use('/artists', artistRouter);
 app.use('/', recordRouter);
 
 // CONNECT TO DB
-// mongoose.connect(
-//     process.env.DB_CONNECTION,
-//     { useNewUrlParser: true, useUnifiedTopology: true },
-//     () => {console.log("Connected to DB!"); console.log(c)}
-// )
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {console.log("Connected to DB!"); console.log(c)}
+)
 
 
-const { MongoClient } = require('mongodb');
-const uri = process.env.DB_CONNECTION;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("connected to DB")
+// const { MongoClient } = require('mongodb');
+// const uri = process.env.DB_CONNECTION;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   console.log("connected to DB")
 
-  // perform actions on the collection object
-  client.close();
-});
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 
 // LISTEN TO SERVER
